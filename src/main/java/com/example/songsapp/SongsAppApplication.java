fb4fbc79341f11f960f62204cf62134b445c32c2
shaiedri <shaiedri850@gmail.com>
2023-05-21 21:57:22 +0300

    initializer: data-jpa,devtools,thymeleaf,web,actuator,h2,lombok Closes #1

---
+++ src/main/java/com/example/songsapp/SongsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.songsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/songsapp/SongsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.songsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
