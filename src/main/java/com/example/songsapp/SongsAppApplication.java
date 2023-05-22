2466ff2d50b30f33d767b16c67110788485586d1
shaiedri <shaiedri850@gmail.com>
2023-05-22 14:32:32 +0300

    Open project and get beans from IOC Closes #2

--- src/main/java/com/example/songsapp/SongsAppApplication.java
+++ src/main/java/com/example/songsapp/SongsAppApplication.java
@@ -2,12 +2,18 @@ package com.example.songsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SongsAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SongsAppApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SongsAppApplication.class, args);
+		String[] bNames = applicationContext.getBeanDefinitionNames();
+		System.out.println(applicationContext.getBeanDefinitionCount());
+		Arrays.stream(bNames).forEach(bean-> System.out.println(bean));
 	}
 
 }
